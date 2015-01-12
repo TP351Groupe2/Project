45d3947893e29f3bc0d01bc2ca624bc6b229d620
MageOsiris <gaetan.cordonnier@hotmail.fr>
2015-01-12 13:59:29 +0100

    projet

---
+++ Project/Tower - NeedChemin/Tower - NeedChemin/core/src/Modele/CheminPossible.java
@@ -0,0 +1,27 @@
+package Modele;
+
+import java.util.ArrayList;
+
+import Modele.Monstre.Monstre;
+
+public class CheminPossible {
+	private Terrain T;
+	private Monstre Monstre;
+	private ArrayList<Case> TabChemin;
+	
+	public CheminPossible(){
+		
+	}
+	
+	public void updateChemin(Terrain T,Monstre Monstre,ArrayList<Case> TabChemin){
+		
+		this.Monstre = Monstre;
+		this.T = T;
+		this.TabChemin = TabChemin;
+		int cpt;
+		while(Monstre.getIndexCaseL() == T.getNbCaseLong()-1 ){
+			
+		}	
+		
+	}
+}
